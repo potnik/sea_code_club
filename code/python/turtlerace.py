4949b0f8f420f3b7a7ebd16458a972c718fbc498
potnik <tkintop@gmail.com>
2019-11-13 20:25:13 -0600

    added bob

--- code/python/turtlerace.py
+++ code/python/turtlerace.py
@@ -29,5 +29,14 @@ ada.penup()
 ada.goto(-160, 100)
 ada.pendown()
 
+bob = Turtle()
+bob.color("blue")
+bob.shape("turtle")
+
+bob.penup()
+bob.goto(-160, 70)
+bob.pendown()
+
 for turn in range(100):
     ada.forward(randint(1, 5))
+    bob.forward(randint(1, 5))

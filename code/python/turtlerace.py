ec2156917b9932e6cfa77633773eb078ee279160
potnik <tkintop@gmail.com>
2019-11-13 20:22:35 -0600

    added ada

--- code/python/turtlerace.py
+++ code/python/turtlerace.py
@@ -1,9 +1,8 @@
 #!/bin/python3
 
-# pylint: disable-msg=E0611
-
-from turtle import speed, penup, goto, write, right, pendown, forward, left, backward
-# from random import randint
+from turtle import speed, penup, goto, write, \
+    right, pendown, forward, left, backward, Turtle
+from random import randint
 
 speed(0)
 penup()
@@ -21,3 +20,14 @@ for step in range(15):
     backward(160)
     left(90)
     forward(20)
+
+ada = Turtle()
+ada.color("red")
+ada.shape("turtle")
+
+ada.penup()
+ada.goto(-160, 100)
+ada.pendown()
+
+for turn in range(100):
+    ada.forward(randint(1, 5))
